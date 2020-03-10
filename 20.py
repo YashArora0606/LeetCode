fe441b0e278a4cfd832376f1eb8ea541d2d2f062
Yash Arora <YashArora0606@users.noreply.github.com>
2020-03-10 01:05:55 -0400

    Create 20.py

---
+++ 20.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        brackets = {"}":"{", "]":"[", ")":"("}
+        stack = []
+        for b in s:
+            if b in brackets.values():
+                stack.append(b)
+            else:
+                if len(stack) == 0:
+                    return False
+                elif stack[len(stack) - 1] == brackets[b]:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
+
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
