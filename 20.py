651b22dcfd7d012452d857f34e4ae2d7f7394c60
Yash Arora <YashArora0606@users.noreply.github.com>
2020-03-10 20:35:54 -0400

    Update 20.py

--- 20.py
+++ 20.py
@@ -1,18 +1,18 @@
 class Solution(object):
-    def isValid(self, s):
-        brackets = {"}":"{", "]":"[", ")":"("}
-        stack = []
-        for b in s:
-            if b in brackets.values():
-                stack.append(b)
-            else:
-                if len(stack) == 0:
-                    return False
-                elif stack[len(stack) - 1] == brackets[b]:
-                    stack.pop()
+        def isValid(self, s):
+            brackets = {"}":"{", "]":"[", ")":"("}
+            stack = []
+            for b in s:
+                if b in brackets.values():
+                    stack.append(b)
                 else:
-                    return False
-        return len(stack) == 0
+                    if len(stack) == 0:
+                        return False
+                    elif stack[len(stack) - 1] == brackets[b]:
+                        stack.pop()
+                    else:
+                        return False
+            return len(stack) == 0
 
 #         """
 #         :type s: str
