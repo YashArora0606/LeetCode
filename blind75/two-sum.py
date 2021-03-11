ebfb14733d8e4a6cea765975ad2b0e085381bd3b
Yash Arora <31422627+YashArora0606@users.noreply.github.com>
2021-03-11 14:15:58 -0500

    refactor

---
+++ blind75/two-sum.py
@@ -0,0 +1,9 @@
+# Hashmap solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numToIndex = {}
+        for i in range(len(nums)):
+            if target - nums[i] in numToIndex:
+                return [numToIndex[target - nums[i]], i]
+            else:
+                numToIndex[nums[i]] = i
